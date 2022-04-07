dc6629a3a0defe4a292e1286dcea5370738d5a5d
davide <da>
2022-04-07 09:35:06 +0200

    Grande Gero aiutaci che go sbaja committare

---
+++ src/camorraCinese.java
@@ -0,0 +1,10 @@
+
+
+public class camorraCinese {
+    public static void main(String[] args) {
+        //Simulare con 2 tred il gioco della camorraCinese
+        //(estraggo un numero casuale tra 1 e 10 e chi ha il numero maggiore vince)
+        //il gioco termina dopo 5 round vinti
+        //visualizzare i dati e vincitore di ogni round
+    }
+}
